8dccb7a22685712492aa6d3955ad3685d0aea35e
2539847 <2539847@cegeplimoilou.ca>
2026-03-30 13:17:38 -0400

    ajout méthode de CircuitParallele

--- APP4 emile/src/electronique/CircuitParallele.java
+++ APP4 emile/src/electronique/CircuitParallele.java
@@ -1,4 +1,12 @@
 package electronique;
 
 public class CircuitParallele extends Circuit{
+    public CircuitParallele(List<Composant>){
+        this.calculerResistance();
+    }
+
+    @Override
+    public double calculerResistance() {
+
+    }
 }
