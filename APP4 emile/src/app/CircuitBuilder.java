9b73bab9252f9136171c84f9a088557e1fdc9437
2539847 <2539847@cegeplimoilou.ca>
2026-04-01 10:30:28 -0400

    ajout builder

--- APP4 emile/src/app/CircuitBuilder.java
+++ APP4 emile/src/app/CircuitBuilder.java
@@ -1,7 +1,17 @@
 package app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import electronique.Composant;
+
 public class CircuitBuilder {
     public CircuitBuilder(){
 
+    }
+
+
+    private Composant lireComposant(JsonNode node){
+        String type = node.get("type").asText();
+
+        if("")
     }
 }
